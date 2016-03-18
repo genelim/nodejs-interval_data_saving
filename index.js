6e90ee9abc3934f2599618ba37c23b675e50865d
Gene Lim <Gene Lim>
2016-03-18 09:38:19 +0800

    added distance canculation

--- index.js
+++ index.js
@@ -13,4 +13,26 @@ setInterval(function(){
         if (err) throw err;
         console.log('User created!');
     });
+    
+    //between gurney plaza to relau about 12-13km
+    console.log(getDistanceFromLatLonInKm(5.26,100.18, 5.329576,100.272526));
+    
 }, 1000);
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+        ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI/180)
+}
